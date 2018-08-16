7aa12de57a0e64ff79114bae486b7fde6a678d6c
Abhay <abhayk97@gmail.com>
2018-08-16 22:10:08 +0530

    setting textContent

--- domExercies.js
+++ domExercies.js
@@ -1,2 +1,9 @@
 var h1 = document.querySelector("h1");
 h1.classList.toggle("red");
+
+var p =document.querySelector("#last");
+var text = p.innerHTML;
+console.log(text);
+
+var ul =document.querySelector("ul");
+ul.innerHTML = "this text has just been replaced!";
